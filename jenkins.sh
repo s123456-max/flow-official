92469f45f5c4b180f68cff1eae6794ed00db556e
root <root@C20230319143154.local>
2023-03-20 08:11:12 -0400

    jenkins部署配置

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,6 @@
-echo "清理已有容器及镜像资源"
-    for container in {"gateway"}
-do
-    if docker ps | grep ${container} ;then
-        docker stop ${container}
-    fi
-    if docker ps -a | grep ${container};then
-        docker rm ${container}
-    fi
-    if docker images | grep flow/${image};then
-        docker rmi flow/${image}
-    fi
-done
+#!/bin/bash
+if docker ps | grep gateway; then
+    docker stop gateway
+    docker rm gateway
+    docker rmi flow/gateway:latest
+fi
