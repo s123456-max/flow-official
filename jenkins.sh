ea26711b4760c809003a51984ec6bb2c194eaf34
AlexMisko <1586924294@qq.com>
2023-03-20 12:31:11 +0000

    更新 'jenkins.sh'

--- jenkins.sh
+++ jenkins.sh
@@ -4,3 +4,23 @@ if docker ps | grep gateway; then
     docker rm gateway
     docker rmi flow/gateway:latest
 fi
+if docker ps | grep auth; then
+    docker stop auth
+    docker rm auth
+    docker rmi flow/auth:latest
+fi
+if docker ps | grep video; then
+    docker stop video
+    docker rm video
+    docker rmi flow/video:latest
+fi
+if docker ps | grep userinfo; then
+    docker stop userinfo
+    docker rm userinfo
+    docker rmi flow/userinfo:latest
+fi
+if docker ps | grep comment; then
+    docker stop comment
+    docker rm comment
+    docker rmi flow/comment:latest
+fi
