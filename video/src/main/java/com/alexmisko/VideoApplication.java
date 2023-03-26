b602b9ebe87f03f1d708c7428c47b9cd45945798
AlexMisko <1586924294@qq.com>
2023-03-26 01:04:39 +0000

    更新 'video/src/main/java/com/alexmisko/VideoApplication.java'

--- video/src/main/java/com/alexmisko/VideoApplication.java
+++ video/src/main/java/com/alexmisko/VideoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableNacosDiscovery
 public class VideoApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoApplication.class, args);
