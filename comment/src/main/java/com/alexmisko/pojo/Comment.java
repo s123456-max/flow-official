bdcc09a425a56dceec1d177dab8e4260ce178a6b
root <root@CTSALR0Y4qXoa.local>
2023-04-07 10:05:35 +0000

    提交初版

--- comment/src/main/java/com/alexmisko/pojo/Comment.java
+++ comment/src/main/java/com/alexmisko/pojo/Comment.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 import java.util.Date;
@@ -25,9 +27,11 @@ public class Comment {
     private Long parentId;
 
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private Date createTime;
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private Date updateTime;
 
     @TableField(exist = false)
